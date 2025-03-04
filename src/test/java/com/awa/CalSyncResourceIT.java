8a45ee61af8dc0277509fa984f5f4099b72e134f
Fernando Ortega <fernandoog@gmail.com>
2025-03-04 22:29:59 +0100

    start

---
+++ src/test/java/com/awa/CalSyncResourceIT.java
@@ -0,0 +1,8 @@
+package com.awa;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class CalSyncResourceIT extends CalSyncResourceTest {
+    // Execute the same tests but in packaged mode.
+}
